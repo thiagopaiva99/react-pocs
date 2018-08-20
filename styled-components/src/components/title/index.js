1e49a1bd4e3324129677fa50e6b075016c32d7d0
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-08-20 14:36:03 -0300

    Creating a title with styled-components

---
+++ styled-components/src/components/title/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+export default props => (
+  <Wrapper>
+    <Title>{props.children}</Title>
+  </Wrapper>
+);
