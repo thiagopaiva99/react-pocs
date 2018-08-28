1ec8becca064d5ebdf6215e3b16e89a4bfe3d37e
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-08-28 13:03:47 -0300

    POC of nuka carousel

---
+++ nuka-carousel/src/components/slider/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Carousel from 'nuka-carousel';
+
+export default class Slider extends React.Component {
+  render() {
+    const settings = {
+      slidesToShow: 2,
+      slidesToScroll: 2
+    };
+
+    return (
+      <Carousel {...settings}>
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1" />
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2" />
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3" />
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4" />
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5" />
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6" />
+      </Carousel>
+    );
+  }
+}
